80b14f28152aa67b8a322e5c1f348473f48d93ea
oangel26 <oa.angel26@gmail.com>
2021-11-07 14:55:29 -0500

    PEP8 correction

--- models/review.py
+++ models/review.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Module that defines all common attributes/methods for other classes """
 from models.base_model import BaseModel
+from models.place import Place
+from models.user import User
 
 
 class Review(BaseModel):
