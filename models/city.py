775b043d6f898df01ef7b57ccbe610d5502f24ad
oangel26 <oa.angel26@gmail.com>
2021-11-07 14:51:42 -0500

    PEP8 correction

--- models/city.py
+++ models/city.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Module that defines all common attributes/methods for other classes """
-from models import state
+from models import State
 from models.base_model import BaseModel
+
 
 class City(BaseModel):
     """Class that represents model of state"""
