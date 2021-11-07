8f70b8fe9b53d37628631159f385f3b0e8d24baa
Mapu456 <mpaula456@hotmail.com>
2021-11-07 11:47:55 -0500

    update advanced points

--- models/city.py
+++ models/city.py
@@ -2,12 +2,8 @@
 """Module that defines all common attributes/methods for other classes """
 from models import state
 from models.base_model import BaseModel
-from models.state import State
 
 class City(BaseModel):
     """Class that represents model of state"""
-    if State.id is not None:
-        state_id = State.id
-    else:
-        state_id = ""
+    state_id = ""
     name = ""
