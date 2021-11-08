2042bfd804e5bb5a6807fe9b1c35c6147f5d3635
Mapu456 <mpaula456@hotmail.com>
2021-11-08 16:46:22 -0500

    test function all of filestorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,6 +5,7 @@
 import unittest
 from models.engine.file_storage import FileStorage
 from models import storage
+from models.base_model import BaseModel
 
 
 class TestFileStorage(unittest.TestCase):
@@ -13,6 +14,19 @@ class TestFileStorage(unittest.TestCase):
     def setUp(self):
         """create a instance"""
         self.new_file_storage = FileStorage()
+
+    def test_function_all(self):
+        """ this function should return the dictionary __objects
+        """
+        setattr(FileStorage, "_FileStorage__objects", {})
+
+        instance = FileStorage()
+        self.assertEqual(instance.all(), {})
+
+        setattr(FileStorage, "_FileStorage__objects", {"Hola", "Mundo"})
+        self.assertEqual(instance.all(), {"Hola", "Mundo"})
+        setattr(FileStorage, "_FileStorage__objects", {})
+
 
 if __name__ == "__main__":
     unittest.main()
